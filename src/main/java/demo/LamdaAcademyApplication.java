209c2b7ead63ec30153c0fce1ce69698c923ff6f
Houssem ZAIER <Houssem.Zaier@gmail.com>
2015-09-17 10:44:12 +0200

    first commit

---
+++ src/main/java/demo/LamdaAcademyApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamdaAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LamdaAcademyApplication.class, args);
+    }
+}
